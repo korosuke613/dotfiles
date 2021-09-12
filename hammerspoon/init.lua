505745baffd21ff5edc1b833d3f9738eba326349
Futa HIRAKOBA <korosuke613613@gmail.com>
2021-09-13 02:21:57 +0900

    fix: hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
 hs.hotkey.bind({"cmd"}, "t", function()
+	hs.application.enableSpotlightForNameSearches(true)
     local iterm2 = hs.application.get("iTerm2")
     if iterm2 == nil then
         hs.application.launchOrFocus("/Applications/iTerm.app")
