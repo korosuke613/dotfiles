18bae7ad2d13984e5e711808ec3a512ff1cab4e8
github-action-benchmark <github@users.noreply.github.com>
2021-11-28 17:37:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f3a53641081cb0d41d579f790104d69965b7c318

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638120713939,
+  "lastUpdate": 1638121071562,
   "repoUrl": "https://github.com/korosuke613/dotfiles",
   "entries": {
     "Benchmark": [
@@ -57,6 +57,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/korosuke613/dotfiles/commit/eb82ba3081d3cba5ea7e152bc9bb2ea41bdf826d"
         },
         "date": 1638120712813,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 0.00322581,
+            "unit": "Second"
+          },
+          {
+            "name": "zsh install time",
+            "value": 0.01,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "korosuke613613@gmail.com",
+            "name": "Futa HIRAKOBA",
+            "username": "korosuke613"
+          },
+          "committer": {
+            "email": "korosuke613613@gmail.com",
+            "name": "Futa HIRAKOBA",
+            "username": "korosuke613"
+          },
+          "distinct": true,
+          "id": "f3a53641081cb0d41d579f790104d69965b7c318",
+          "message": "wip: 2021/11/29 02:37:05\n\n .github/workflows/benchmark.yaml | 3 ++-\n 1 file changed, 2 insertions(+), 1 deletion(-)",
+          "timestamp": "2021-11-29T02:37:05+09:00",
+          "tree_id": "068f892a5fc0885ec63208a43f93c31fa5e32f58",
+          "url": "https://github.com/korosuke613/dotfiles/commit/f3a53641081cb0d41d579f790104d69965b7c318"
+        },
+        "date": 1638121070555,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
