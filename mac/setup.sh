b885e746bb00f7cd82a68d80eecd197b683bceab
Futa HIRAKOBA <korosuke613@users.noreply.github.com>
2022-02-25 15:57:00 +0900

    fix: brew setup

--- mac/setup.sh
+++ mac/setup.sh
@@ -5,6 +5,8 @@ if [ -z "$(command -v brew)" ]; then
     echo "--- Install Homebrew is Start! ---"
 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
     brew bundle
 
     echo "--- Install Homebrew is Done!  ---"
