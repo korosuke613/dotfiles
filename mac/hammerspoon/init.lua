0c979e4c8b1bbb0d27e58560b4c625ee0715bfad
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-08-03 16:56:19 +0900

    fix: use iTerm2

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -1,24 +1,24 @@
--- hs.hotkey.bind({"cmd"}, "t", function()
--- 	hs.application.enableSpotlightForNameSearches(true)
---     local iterm2 = hs.application.get("iTerm2")
---     if iterm2 == nil then
---         hs.application.launchOrFocus("/Applications/iTerm.app")
---     elseif iterm2:isFrontmost() then
---         iterm2:hide()
---     else
---         hs.application.launchOrFocus("/Applications/iTerm.app")
---     end
--- end)
-
 hs.hotkey.bind({"cmd"}, "t", function()
 	hs.application.enableSpotlightForNameSearches(true)
-    local warp = hs.application.get("Warp")
-    if warp == nil then
-        hs.application.launchOrFocus("/Applications/Warp.app")
-    elseif warp:isFrontmost() then
-        warp:hide()
+    local iterm2 = hs.application.get("iTerm2")
+    if iterm2 == nil then
+        hs.application.launchOrFocus("/Applications/iTerm.app")
+    elseif iterm2:isFrontmost() then
+        iterm2:hide()
     else
-        hs.application.launchOrFocus("/Applications/Warp.app")
+        hs.application.launchOrFocus("/Applications/iTerm.app")
     end
 end)
+
+-- hs.hotkey.bind({"cmd"}, "t", function()
+-- 	hs.application.enableSpotlightForNameSearches(true)
+--     local warp = hs.application.get("Warp")
+--     if warp == nil then
+--         hs.application.launchOrFocus("/Applications/Warp.app")
+--     elseif warp:isFrontmost() then
+--         warp:hide()
+--     else
+--         hs.application.launchOrFocus("/Applications/Warp.app")
+--     end
+-- end)
 
